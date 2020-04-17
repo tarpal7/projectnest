785411677b1db41a3ec59a300620e88ed0ba63e7
Pavlo Taradayka <tarpal7@gmail.com>
2020-04-17 16:15:15 -0400

    added module todo

---
+++ src/modules/todo/entities/todo.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  task: string;
+
+  @Column({default: false})
+  isComplited: boolean;
+  
+}
